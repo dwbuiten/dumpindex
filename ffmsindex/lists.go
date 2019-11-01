f170333c4115c1a4fab4c64abf165d3b817c5f5a
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2019-11-01 15:01:42 +0000

    Implement a function to get keyframe positions within a tracks frame slice
    This is useful for, e.g. looking up a keyframe's PTS.
    
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- ffmsindex/lists.go
+++ ffmsindex/lists.go
@@ -18,6 +18,24 @@ func (t *Track) GetKeyframes() []uint {
 	return ret
 }
 
+// GetKeyframeIndicies gets a list of indicies that can be used in
+// a given Track's frame slice. This differs from GetKeyframes in
+// that it takes into account all frames, and not only visible frames.
+// GetKeyframes would be more suitable, to, for example, use for
+// seeking with FFMS2's API, or measuring keyframe distances.
+func (t *Track) GetKeyframeIndices() []uint {
+	// frame/24 is an arbitrary number to start allocating for
+	ret := make([]uint, 0, len(t.Frames)/24)
+
+	for _, v := range t.visibleFrames {
+		if t.Frames[v].KeyFrame {
+			ret = append(ret, uint(v))
+		}
+	}
+
+	return ret
+}
+
 // GetTimestamps returns a list of timestamps for visible frames.
 // The number of timestamps may differ from the length of the Frames
 // slice, since some codecs, such as VP8, have hidden frames. This
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/dwbuiten/dumpindex/v5
+
+go 1.12
